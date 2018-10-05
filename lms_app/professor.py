86a1e04ea42cc63d6c62c24f7b6dfbb916902088
Johnny.Anthony7 <johnny.anthony.silva@outlook.com>
2018-10-05 08:39:00 -0300

    Teste 15

--- lms_app/professor.py
+++ lms_app/professor.py
@@ -5,17 +5,14 @@ class Professor(models.Model):
         return self.nome + self.email
     def save(self):
     	if self.login == "":
-    		raise Exception('Login não informado')
+    		raise Exception('')
     	if self.email == "":
     		self.email = 'email nao fornecido'
     	if len(Professor.objects.filter(login=self.login))!=0:
     		raise Exception("")
     	super(Professor,self).save()
-
-        
     nome = models.TextField(max_length=255)
     email = models.TextField(max_length=255)
     celular = models.TextField(max_length=20)
     login = models.TextField(max_length=20)
     senha = models.TextField(max_length=20)
-    
