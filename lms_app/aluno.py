0bc67f6b9e4fd9bd019077bbf1a69239eae5347f
Johnny.Anthony7 <johnny.anthony.silva@outlook.com>
2018-10-05 07:52:48 -0300

    Passando mais testes com o grupo

--- lms_app/aluno.py
+++ lms_app/aluno.py
@@ -1,22 +1,19 @@
 from django.db import models
-from lms_app.professor import Professor
+from lms_app.professor import *
 
 class Aluno(models.Model):
-    def __str__(self):
-        return self.nome + self.email
     def save(self):
-    	if self.login == "":
-    		raise Exception('')
-    	if self.email == "":
-    	    self.email = 'email nao fornecido'
-    	if len(Aluno.objects.filter(login=self.login)!=0:
-    		raise Exception("")
-    	super(Aluno,self).save()
-    if len(Professor.objects.filter(login=self.login))==if len(Aluno.objects.filter(login=self.login):
-    		raise Exception("")
-        
+     	if self.email == "":
+     		self.email =  'email nao fornecido'
+     	if self.login=='':
+     		raise Exception('')
+     	if len(Aluno.objects.filter(login=self.login))>0:
+     		raise Exception('')
+     	if len(Professor.objects.filter(login=self.login)) == len(Aluno.objects.filter(login=self.login)) and len(Aluno.objects.filter(login=self.login)) > 1 or len(Professor.objects.filter(login=self.login)):
+     		raise Exception("")
+     	super(Aluno,self).save()
     nome = models.TextField(max_length=255)
-    email = models.TextField(max_length=255)
+    email =models.TextField(max_length=255)
     celular = models.TextField(max_length=20)
     login = models.TextField(max_length=20)
     senha = models.TextField(max_length=20)
