f873cb7869bf5c52fd8d8a5f18582d916a1866ad
Johnny.Anthony7 <johnny.anthony.silva@outlook.com>
2018-10-24 08:37:02 -0300

    HTML e CSS funcionando no servidor Django

--- lms_app/views.py
+++ lms_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, ListView
 
-# Create your views here.
+
+def home(request):
+        return render(request,'index.html')
+
+def listaCurso(request):
+        return render(request, 'ListadeCursos.html')
+
+def detalhesCurso(request):
+        return render(request, 'DetalhesdeCurso.html')
+
+def login(request):
+        return render(request, 'Login.html')
+
+def noticias(request):
+        return render(request, 'Noticias.html')
+
+def disciplinas(request):
+        return render(request, 'Disciplinas.html')
+
+def esqueceuSenha(request):
+    return render(request, 'Esqueceusenha.html')
+
+
